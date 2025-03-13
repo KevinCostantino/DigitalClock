34c0a8d605585f9d01f63777bb37415d311985be
KevinCostantino <kevincostantino2500@gmail.com>
2025-03-12 22:59:53 -0300

    Deploy para GitHub Pages

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/DigitalClock/", // 🔹 Substitua pelo nome do repositório
-});
